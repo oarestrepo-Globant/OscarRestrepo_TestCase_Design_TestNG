627d3a556c393c265f095259278d338f7ae111e6
oscar.restrepo <oscar.restrepo@globant.com>
2022-11-07 22:18:12 -0500

    suite

--- WebAutomation/src/test/java/com/globant/AppTest.java
+++ WebAutomation/src/test/java/com/globant/AppTest.java
@@ -1,7 +1,16 @@
 package com.globant;
 
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-public class AppTest 
-{
+import java.util.logging.Logger;
 
+// https://www.espnqa.com/?_adbock=true&src=com
+public class AppTest {
+    private static final Logger log = Logger.getLogger(AppTest.class.getName());
+
+    @Test
+   public void login(){
+
+   }
 }
