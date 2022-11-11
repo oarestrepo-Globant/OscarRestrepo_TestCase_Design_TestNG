7e7b7fdd99d8eea67312be42cf9934d60c0a88be
oscar.restrepo <oscar.restrepo@globant.com>
2022-11-10 20:07:49 -0500

    refactor gitIgnore

--- src/test/java/com/globant/ESPNLoginTest.java
+++ src/test/java/com/globant/ESPNLoginTest.java
@@ -11,6 +11,7 @@ public class ESPNLoginTest extends AppTest{
         log.info("En el popup de Login ingresar userName");
         log.info("En el popup de Login ingresar password");
         log.info("Dar click en Login");
+
         log.info("Pararse en el avatar del navBar");
         log.info("Verificar si en la columna izquierda del pop up aparece !Bienvenido userName!");
         log.info("Verificar si en la columna izquierda del pop up esta la opcion de logout");
