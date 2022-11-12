a9287b7c1e5783bce1192aeeb8886a3a8a2bcb7a
oscar.restrepo <oscar.restrepo@globant.com>
2022-11-12 18:50:56 -0500

    WebOperations config

--- src/main/java/org/espn/configuration/WebOperations.java
+++ src/main/java/org/espn/configuration/WebOperations.java
@@ -1,4 +1,48 @@
 package org.espn.configuration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
 public class WebOperations {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public WebOperations(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5L));
+        initElements(driver, this);
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    public void waitForClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void clickElement(WebElement element){
+        waitForClickable(element);
+        element.click();
+    }
+
+    public void typeOnInput(WebElement element, String text){
+        element.sendKeys(text);
+    }
+
+    public void waitForVisibility(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitForVisibility(List<WebElement> elements){
+        wait.until(ExpectedConditions.visibilityOfAllElements(elements));
+    }
+
 }
