f9d9fe62bddfb41914dce6bfed454427bc47c1c6
oscar.restrepo <oscar.restrepo@globant.com>
2022-11-12 17:59:57 -0500

    Driver Config

--- src/main/java/org/espn/configuration/Driver.java
+++ src/main/java/org/espn/configuration/Driver.java
@@ -1,8 +1,19 @@
 package org.espn.configuration;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
-    private WebDriver driver;
+  private WebDriver driver;
+
+    public Driver(String browser){
+      WebDriverManager.chromedriver().setup();
+      driver = new ChromeDriver();
+    }
+
+  public WebDriver getDriver() {
+    return driver;
+  }
 }
